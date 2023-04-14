ae4225408ec9a8d2219439c1bf42b3b573ddaf95
Stav42 <vaditya@iitk.ac.in>
2023-04-15 04:02:43 +0530

    stuck on slicing rows

--- active_set.cpp
+++ active_set.cpp
@@ -2,18 +2,29 @@
 #include <eigen3/Eigen/Dense>
 #include <vector>
 
+Eigen::VectorXf arange(double low, double high, double step, bool with_last = false)
+{
+    high -= (with_last) ? 0 : step;
+    int N = static_cast<int>(std::floor((high - low) / step) + 1);
+    return Eigen::VectorXf::LinSpaced(N, low, high);
+}
+
 class Active_set{
 
     public:
         Eigen::MatrixXf H; Eigen::MatrixXf A;
         Eigen::VectorXf f; Eigen::VectorXf b;
         
+        // Dual Formulation of the Problem
         Eigen::VectorXf W; Eigen::VectorXf W_hat;
         Eigen::VectorXf lambda; Eigen::MatrixXf R;
 
         Eigen::MatrixXf M; 
         Eigen::VectorXf d;
         Eigen::VectorXf v;
+
+        //Primal Variables
+        Eigen::VectorXf x; 
 
     Active_set(Eigen::MatrixXf H, Eigen::MatrixXf A, Eigen::VectorXf f, Eigen::VectorXf b){
         
@@ -28,6 +39,27 @@ class Active_set{
         Eigen::MatrixXf Rt = this->R.transpose(); 
         this->v = Rt.inverse() * this->f;
         this->d = this->b + this->M * this->v;
+
+    }
+
+    void solve(){
+    
+        this->W = Eigen::VectorXf{0};
+        std::cout<<this->W<<std::endl;
+        this->W_hat = arange(1, this->M.rows(),true);
+        std::cout<<this->W_hat<<std::endl;
+        this->lambda = Eigen::VectorXf::Zero(this->M.rows());
+        this->lambda[0] = 3;
+
+        int k = 0;
+
+        while(true){
+            
+            std::cout<<this->W<<std::endl;   
+            Eigen::MatrixXf M_k = this->M(Eigen::placeholders::all, this->W);
+            std::cout<<M_k<<std::endl;
+            break;
+        }
 
     }
 
@@ -53,7 +85,8 @@ int main(){
     Eigen::VectorXf b{{3.0, 2.0, -2.0}};
 
     Active_set Prob(H, A, f, b);
-    std::cout<<Prob.M<<std::endl;
+
+    Prob.solve();
 
     return 0;
 }
