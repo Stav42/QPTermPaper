a02f19160671f30539131f4295c8d300de48c0fc
Pratikhya Ranjit <pratikhya.ranjit2@gmail.com>
2023-04-22 21:13:53 +0530

    Updated active_set.py

--- active_set.py
+++ active_set.py
@@ -2,25 +2,30 @@ import numpy as np
 from scipy import sparse
 from sksparse.cholmod import cholesky
 
-
-
 class Active_set:
 
     def __init__(self, H, f, A, b):
 
         # Primal Formulation
+        self.m = A.shape[0] #number of constraints
         self.H = H
         self.f = f
         self.A = A
         self.b = b
+        self.max_iter = 8
+        #tolerance
+        self.eps_p = 1e-6
+        self.eps_d = 1e-12
+        self.eps_z = 1e-11 #zero
 
         self.lamda = None
         
         # Dual Formulation
         self.R = np.transpose(np.linalg.cholesky(self.H))
+        self.Rinv = np.linalg.inv(self.R)
+        # print(self.R)
 
         if not np.allclose(self.R, np.triu(self.R)):# check if upper triangular
-            print(self.R)
             print("Not Upper!")
 
         self.M = None; self.v = None; self.d = None;
@@ -32,136 +37,144 @@ class Active_set:
 
     def form_dual_objective(self):
 
-        self.M = np.matmul(self.A, np.linalg.inv(self.R))
-        print("M zzz: ", self.M.shape)
-        self.v = np.matmul(np.transpose(np.linalg.inv(self.R)), self.f)
-        self.d = self.b + np.matmul(self.M, self.v)
+        self.M = self.A.dot(self.Rinv)
+        Rinv_T = np.transpose(self.Rinv)
+        self.v = Rinv_T.dot(self.f)
+        self.d = self.b + self.M.dot(self.v)
 
-
+        # print(self.M)
+        # print(self.v)
+        # print(self.d)
 
 def solve(Prob):
     
     ## Working Set. Will keep increasing with iterations ##
     Prob.W = np.array([0])
-    Prob.W_hat = np.arange(1, Prob.M.shape[0])
-    print(Prob.W_hat.shape)
-    print(Prob.W)
-    Prob.lamda = np.expand_dims(np.zeros(Prob.M.shape[0]), axis=1)
-    Prob.lamda[0] = 3
-    print(Prob.lamda.shape)
-    print("Shapes")
-    k = 0;
+    Prob.W_hat = np.setdiff1d(np.arange(Prob.m),Prob.W)
+    Prob.lamda = np.zeros((Prob.m,1))
+    Prob.lamda[0] = 3.0
+    # print(Prob.W_hat)
+    # print(Prob.W)
+    # print(Prob.lamda)
+    k = 0
+    lamda_k = None
+    lamda_k_W = None
 
-    while True:
-        # print(Prob.d) 
-        M_k = Prob.M[Prob.W, :]
+    while k<Prob.max_iter:
+        print(f'Iteration: {k+1}')
+        M_k = Prob.M[Prob.W]
         # print("M_k: ", M_k)
         d_k = Prob.d[Prob.W]
-        print("d_k: ", d_k)
+        # print("d_k: ", d_k)
 
-        M_k_hat = Prob.M[Prob.W_hat, :]
+        M_k_hat = Prob.M[Prob.W_hat]
         d_k_hat = Prob.d[Prob.W_hat]
-        # print("d_k_hat: ", d_k_hat.shape)
 
         ############ Process ###########
-        
-        # print("M_k * M_K.T: ", np.matmul(M_k, M_k.T))
-        if np.linalg.det(np.matmul(M_k, M_k.T)) != 0:
-            lamda_k = Prob.lamda.copy() 
-            factor = cholesky(sparse.csr_matrix(np.matmul(M_k, M_k.T)))
+        '''
+        MMT = (M_k)(M_k.T)
+        '''
+        MMT = M_k.dot(M_k.T) 
+        if np.linalg.det(MMT) != 0:
+            print(f'Step 3')
+            #create sparse matrix
+            MMT_sparse = sparse.csc_matrix(MMT)
+            if not MMT_sparse.sort_indices:
+                MMT_sparse.sort_indices()
+
+            lamda_k = Prob.lamda.copy() #copy by reference
+            factor = cholesky(MMT_sparse)
             lamda_k_W = factor(-d_k)
-            # lamda_k = factor(-d_k)
 
-            print("W is: ", Prob.W)
-            print("M_k is", M_k)
+            print("W : \n ", Prob.W)
+            print("M_k : \n ", M_k)
             lamda_k[Prob.W] = lamda_k_W
-            print(lamda_k)
-            ############# ISSUES HERE #########################
-            if np.all(lamda_k > 0):
-                print(M_k)
-                print(lamda_k)
-                mu_k = np.matmul(M_k_hat, np.matmul(M_k.transpose, lamda_k)) + d_k_hat
-                self.lamda = lamda_k
+            print(f'lambda_k : \n {lamda_k}')
 
-                if np.all(mu_k > 0):
+            if np.all(lamda_k >= -Prob.eps_d):
+                print(f'Step 5')
+                mu_k_W_hat = M_k_hat.dot(M_k.T.dot(lamda_k_W)) + d_k_hat
+                Prob.lamda = lamda_k
+
+                if np.all(mu_k_W_hat > -Prob.eps_p):
+                    print("Converged!")
                     break
                 else:
-                    j = np.argmin(mu_k)
-                    Prob.W = np.hstack(Prob.W, Prob.W_hat[j])
+                    j = np.argmin(mu_k_W_hat)
+                    Prob.W = np.hstack((Prob.W, j)) #check output
+                    print(f'Step 7: New W \n {Prob.W}')
 
             else:
-                print("chalu: ")
+                print('Step 9')
                 p_k = lamda_k - Prob.lamda
-                p_k[0] = 3;
-                indexes = []
-                for idx, row in enumerate(lamda_k):
-                    if row[0]<0 and idx in Prob.W:
-                        indexes.append(idx)
+                #index included in working set where lamda<0
+                B = [] 
+                for idx in Prob.W:
+                    if lamda_k[idx][0]< Prob.eps_d:
+                        B.append(idx)
 
-                indexes = np.array(indexes)
-                print("indexes: ", indexes)
-                B_w = Prob.W[indexes]
-                
+                B = np.array(B)
+                print("B: ", B)                
 
-                lamda_next, W_next = fix_component(Prob.lamda, Prob.W, B_w, p_k)
+                print('Step 14')
+                lamda_next, W_next = fix_component(Prob.lamda, Prob.W, B, p_k)
                 print("lamda_next: ", lamda_next)
-                Prob.lamda = lamda_next.copy()
-                Prob.W = W_next.copy()
+                Prob.lamda = lamda_next
+                Prob.W = W_next
 
         else:
-            # print(np.linalg.det(np.matmul(M_k, M_k.T)))
-            U, s, V = np.linalg.svd(np.matmul(M_k,  M_k.T))
-            # print(M_k.shape)
-            # print(np.matmul(M_k, M_k.T).shape)
+            print(f'Step 12:')
+            U, s, V = np.linalg.svd(MMT)
             # extract the nullspace from the decomposition
             nullspace = V[np.argwhere(s < 1e-10).flatten()]
-            # print(nullspace)
+            print(nullspace)
             p_k = None
 
             for col in nullspace.T:
                 # print(col)
-                p_k = np.zeros(Prob.lamda.shape[0])
+                p_k = np.zeros(Prob.m)
                 # print(Prob.W)
                 p_k[Prob.W] = col
-                if np.dot(col, p_k) < 0:
-                    p_k_w = vol
+                if col.dot(p_k) < Prob.eps_z:
                     break
             
-            B = np.where(p_k < 0)
-            B_w = Prob.W[B]
+            print(f'Step 13')
+            B = []
+            for idx in Prob.W:
+                if p_k[idx] < Prob.eps_z:
+                    B.append(idx)
+            B = np.array(B)
+            print(f'B : {B}')
 
-            Prob.lamda, Prob.W = fix_component(Prob.lamda, Prob.W, B_w, p_k)
+            Prob.lamda, Prob.W = fix_component(Prob.lamda, Prob.W, B, p_k)
 
         k = k+1
 
-    x = -1 * np.matmul(np.inverse(self.R), (np.matmul(M_k.T, lamda_k) + Prob.v))
+    print(f'Step 16')
+    x = -1 * Prob.Rinv.dot(M_k.T.dot(lamda_k_W) + Prob.v)
     
-    return x, lamda_k, Prob.W
+    return x, Prob.lamda, Prob.W
 
             
 def fix_component(lamda_k, W_k, B, p_k):
     
-    # print("fix Component")
-    print(lamda_k)
-    print(B)
-    lamda_b = lamda_k[B]
-    # p_b = p_k[B]
-
+    print(f'Step 18')
     min_val = np.inf
     j = 0
 
     for i in B:
-        temp = -1 * np.divide(lamda_k[i], p_k[i])
+        temp = -1 * lamda_k[i][0]/p_k[i][0]
         if temp<min_val:
             min_val = temp
             j = i
             
-    print("Purana: ", W_k, j)
-    W_k = W_k[W_k != j]
-    print("Purana2: ", W_k, j)
-    lamda_k = lamda_k - (lamda_k[j]/p_k[j]) * p_k
+    print(f'Old W_k: \n {W_k} \n j = {j}')
+    #removes j from W_k
+    W_new = np.setdiff1d(W_k,j) 
+    print("New W_k", W_new)
+    lamda_new = lamda_k - (lamda_k[j]/p_k[j]) * p_k
 
-    return lamda_k, W_k
+    return lamda_new, W_new
 
 
 
@@ -187,4 +200,4 @@ if __name__ == "__main__":
     prob.form_dual_objective()
     
     ans, lamda, W = solve(prob)
-    print(ans)
+    print(f'Solution: \n {ans}')
